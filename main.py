9199085221f825f5fe619bbe8d75d56b1f284e7d
Cristhian <cristhian.delgado.2006@gmail.com>
2025-10-26 11:19:20 -0500

    Creacion de la clase estudiante

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #from views.menu_inicio import menu_inicio
 #from views.Iniciar_Sesion import inicio_sesion
-from views.admin.Admin_menu import adminVentana
+from views.estudiante_menu import estudianteventana
 from config.iconos import Iconos
 
 if __name__ == "__main__":
     iconos = Iconos()
-    app = adminVentana(iconos=iconos)
+    app = estudianteventana(iconos=iconos)
     app.run()
+    
---
+++ views/estudiante_menu.py
@@ -0,0 +1,75 @@
+from utils.tkinter import ventana_default, bloqueo_pantalla_completa_default, ventana_modal
+from utils.tkinter import PhotoImage, messagebox, ttk
+from utils.tkinter import *
+import tkinter as tk
+from abc import ABCMeta, abstractmethod
+import os, json
+
+class estudianteventana(ventana_default):
+    def __init__(self, master=None, iconos=None):
+        super().__init__(titulo="GIPU - Bienvenido ", ancho=800, alto=600, iconos= iconos)
+        self.logo = None
+        self.inicio()
+        
+    def inicio(self):
+        # Frame principal
+        frame = ttk.Frame(self, padding=30)
+        frame.pack(expand=True, fill="both")
+
+        # ─────────────────────────────
+        # ENCABEZADO
+        # ─────────────────────────────
+        header = ttk.Frame(frame)
+        header.pack(fill="x", pady=10)
+
+        # Cargar logo
+        logo_path = os.path.join("assets", "img", "Logo_GIPU.png")
+
+        if os.path.exists(logo_path):
+            try:
+                self.logo = tk.PhotoImage(file=logo_path)
+                self.logo = self.logo.subsample(7, 7)
+                logo_label = ttk.Label(header, image=self.logo)
+                logo_label.pack(side="left", padx=(10, 15))
+            except Exception as e:
+                print("Error cargando imagen:", e)
+        # fallback: mostrar texto o un label vacío
+                ttk.Label(header, text="[Logo]").pack(side="left", padx=(10,15))
+        else:
+            # ruta no existe: fallback visible en la UI para que sepas que no hay imagen
+            ttk.Label(header, text="[Logo no encontrado]").pack(side="left", padx=(10,15))
+
+
+        # Título del sistema
+        titulo_frame = ttk.Frame(header)
+        titulo_frame.pack(side="left", anchor="center", pady=10)
+        ttk.Label(titulo_frame, text="GIPU", font=("Arial", 20, "bold"), foreground="#2a4f80").pack(anchor="w")
+        ttk.Label(titulo_frame, text="Bienvenido", font=("Arial", 18), foreground="#2a4f80").pack(anchor="w")
+        boton_frame = ttk.Frame(frame)
+        boton_frame.pack(side="left", anchor="ne", pady=0, padx=10)
+        boton1 = tk.Button(boton_frame, text="Verificar registro unico", font=("Arial", 12), command=self.abrir_modal)
+        boton1.pack(side="left", padx=5)
+        boton2 = tk.Button(boton_frame, text="Añadir postulacion", font=("Arial", 12), command=self.abrir_modal)
+        boton2.pack(side="left", padx=5)
+        boton3 = tk.Button(boton_frame, text="Salir", font=("Arial", 12), command=self.abrir_modal)
+        boton3.pack(side="left", padx=5)
+        
+
+    def abrir_modal(self):
+        herramienta_modal(self)
+class herramienta_modal(ventana_modal,bloqueo_pantalla_completa_default):
+
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.agregar_postulacion
+        self.verificar_registrounico()
+        self.salir()
+
+    def agregar_postulacion(self ):
+
+        print("d")
+    
+    def verificar_registrounico(self):
+        print("d")
+    def salir():
+        print("d")
